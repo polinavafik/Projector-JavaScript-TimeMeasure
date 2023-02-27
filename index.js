4efdeaf9ff7981c7898f4c3d9774e437dbba4912
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-02-27 13:18:35 +0200

    new

---
+++ index.js
@@ -0,0 +1,5 @@
+/*var dateControl = document.querySelector('input[type="date"]');
+dateControl.value = '2017-06-01';
+console.log(dateControl.value); // prints "2017-06-01"
+console.log(dateControl.valueAsNumber); // prints 1496275200000, a JavaScript timestamp (ms)
+*/
